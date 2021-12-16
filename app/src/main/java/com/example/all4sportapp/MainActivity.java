cdb5a11bc43376004a3e68e078904647ff6211cd
Your Name <BeNyEz17>
2021-12-16 15:26:37 +0100

    start

--- app/src/main/java/com/example/all4sportapp/MainActivity.java
+++ app/src/main/java/com/example/all4sportapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.all4sportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btncon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            this.btncon = (Button) findViewById(R.id.btnconnexion);
+
+        this.btncon.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/all4sportapp/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.all4sportapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
