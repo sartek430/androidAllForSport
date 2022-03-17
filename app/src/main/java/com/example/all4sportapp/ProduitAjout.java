16175884740e03e9ab7b603acf5aa1fb586c73fb
PierreTembremande <pierre.tembremande@gmail.com>
2022-03-17 16:23:17 +0100

    ProduitAjout vers API

--- app/src/main/java/com/example/all4sportapp/ProduitAjout.java
+++ app/src/main/java/com/example/all4sportapp/ProduitAjout.java
@@ -14,12 +14,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ProduitAjout extends AppCompatActivity {
     //initialise variable
     Button btScan;
     Button btRefresh;
     Button btAjout;
     String resultat;
+    String line;
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -30,13 +38,6 @@ public class ProduitAjout extends AppCompatActivity {
         btScan = findViewById(R.id.bt_scan);
         btRefresh = findViewById(R.id.button);
         btAjout = findViewById(R.id.button2);
-
-        btAjout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
         btScan.setOnClickListener(new View.OnClickListener(){
 
@@ -65,6 +66,26 @@ public class ProduitAjout extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(ProduitAjout.this, Localisation.class);
                 startActivity(intent);
+            }
+        });
+
+        btAjout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                URL url;
+
+                try {
+                    url = new URL("http://192.168.43.2/all4sport/API/gestionStock.php?reference="+resultat+"&entrepot="+resultat+"&quantite="+resultat);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    line = rd.readLine();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
         }
